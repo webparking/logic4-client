096b0d713b0b38244bac3034991d578951eb0714
Luke Reijmerink <luke@byluke.nl>
2024-01-09 12:02:56 +0100

    Added workflow for coverage check, dependabot and auto update our endpoints

--- generate.php
+++ generate.php
@@ -10,6 +10,6 @@ require_once __DIR__.'/vendor/autoload.php';
 Debug::enable();
 ErrorHandler::register();
 
-$generator = new \Webparking\Logic4Client\Generator\Generator();
+$generator = new Webparking\Logic4Client\Generator\Generator();
 $generator->setRefresh(in_array('--refresh', $argv ?? [], true));
 echo $generator->generate();
