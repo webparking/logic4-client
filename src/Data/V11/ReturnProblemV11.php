bdd9890cea255958e4e8bd998e12ccae410ad68d
lreijmer <132908227+lreijmer@users.noreply.github.com>
2024-09-06 09:32:51 +0000

    Update endpoints

--- src/Data/V11/ReturnProblemV11.php
+++ src/Data/V11/ReturnProblemV11.php
@@ -4,9 +4,11 @@ declare(strict_types=1);
 
 namespace Webparking\Logic4Client\Data\V11;
 
-class ReturnProblem
+class ReturnProblemV11
 {
+    /** @param array<\Webparking\Logic4Client\Data\V11\ReturnSolution> $solutions */
     public function __construct(
+        public ?array $solutions,
         public int $id,
         public ?string $name,
         public ?string $description,
@@ -17,6 +19,7 @@ class ReturnProblem
     public static function make(array $data): self
     {
         return new self(
+            solutions: array_map(static fn (array $item) => ReturnSolution::make($item), $data['Solutions'] ?? []),
             id: $data['Id'] ?? 0,
             name: $data['Name'] ?? null,
             description: $data['Description'] ?? null,
